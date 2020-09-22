8e7ef80f49c5d26ae1232f15af1ad3d6ae506cc8
Aljosa12 <lgaljo@gmail.com>
2020-09-22 20:08:25 +0200

    Update

---
+++ src/source.js
@@ -0,0 +1,11 @@
+import flatpickr from  "flatpickr"
+
+flatpickr("#myInput", {});
+
+/*let today = new Date();
+
+let year = today.getFullYear();
+
+let el = document.getElementById("copyright")
+
+el.innerHTML = '<p> ' + year + today +'</p>'*/
