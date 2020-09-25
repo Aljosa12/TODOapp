32623918e5ff30b77e604040180ac5a526289660
Aljosa12 <lgaljo@gmail.com>
2020-09-25 18:32:23 +0200

    Update

--- models/images.py
+++ models/images.py
@@ -4,8 +4,7 @@ from datetime import datetime
 
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    image = db.Column(db.LargeBinary)
+    image_url = db.Column(db.String)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     author = db.relationship("User")
     created = db.Column(db.String, default=datetime.utcnow)
