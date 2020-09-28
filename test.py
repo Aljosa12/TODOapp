f44be2fc3e95ac5ee902be5c9ec8570ab22a6a25
Aljosa12 <lgaljo@gmail.com>
2020-09-28 22:25:21 +0200

    Update: function for: from day numbers to name of days

---
+++ test.py
@@ -0,0 +1,19 @@
+
+def dayNameFromWeekday(weekday):
+    if weekday == 0:
+        return "Monday"
+    if weekday == 1:
+        return "Tuesday"
+    if weekday == 2:
+        return "Wednesday"
+    if weekday == 3:
+        return "Thursday"
+    if weekday == 4:
+        return "Friday"
+    if weekday == 5:
+        return "Saturday"
+    if weekday == 6:
+        return "Sunday"
+
+
+
